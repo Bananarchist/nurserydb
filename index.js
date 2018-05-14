00288a352cdcfb316377b3c234daaf96c76f73f5
Zach Holker <drsturm@gmail.com>
2018-05-14 14:09:52 -0700

    Learning to Procfile

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routing = require("./server/http");
 const app = express();
+var port = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
@@ -9,6 +10,6 @@ app.use("/collection", routing.collection_routing);
 app.use("/species", routing.species_routing);
 app.use("/category", routing.category_routing);
 
-app.listen(8080, () => {
-    console.log("Server running @ http://localhost:8080");
+app.listen(port, () => {
+    console.log(`Server running @ http://localhost:${port}`);
 });
